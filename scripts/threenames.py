78b518ac1d65d45fc266e9765dde9451aebd1fd3
Jimmy E. Touma <aitatanit@gmail.com>
2014-01-12 15:29:27 -0600

    added lecture 02

---
+++ scripts/threenames.py
@@ -0,0 +1,10 @@
+a = 'dead'                      # Define three attributes
+b = 'parrot'                    # Exported to other files
+c = 'sketch'
+
+
+print(a, b, c)                  # Also used in this file
+
+print a,b,c
+
+print a
