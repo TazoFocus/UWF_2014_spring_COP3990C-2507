5dd9d7eee45e77d6f37d41f729b34d61b83581cd
Jimmy E. Touma <aitatanit@gmail.com>
2014-01-22 15:26:26 -0600

    Moved the scripts folder to the notebooks folder

---
+++ notebooks/scripts/test_script.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+'''
+Name: 
+Course:
+Assignment:
+
+Run: python hw01 100 0 1000
+'''
+# this is a one line comment
+'''
+I do ...
+'''
+
+import random
+
+
+
+#initialize the mean
+mean = 0.0
+
+# list of random number 
